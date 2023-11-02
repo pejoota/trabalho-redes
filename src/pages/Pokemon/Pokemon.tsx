95a4032b13d88b58e9a73ada537a3d05f84cff83
pejoota <pjvcruz1@gmail.com>
2023-11-02 16:46:17 -0300

    acrescentando sprites e características

--- src/pages/Pokemon/Pokemon.tsx
+++ src/pages/Pokemon/Pokemon.tsx
@@ -1,15 +1,32 @@
 import { useState } from 'react';
-import './Pokemon.css'
 import { useFetch } from 'use-http';
+import './Pokemon.css'
 
 export default function Pokemon() {
     const baseUrl = 'https://pokeapi.co/api/v2'
-    const [pokemon, setPokemon] = useState('pikachu');
-    const { loading, error, data = []} = useFetch(`${baseUrl}/pokemon-species/${pokemon}`, {}, [pokemon])
-    console.log(data)
+    const [pokemon, setPokemon] = useState('bulbasaur');
+    const { loading, error, data = []} = useFetch(`${baseUrl}/pokemon/${pokemon}`, {}, [pokemon])
 
-    return(
-        <div className="Pokemon">
-        </div>
-    );
+    if(!error && !loading){ 
+
+        return(
+            <div className="Pokemon">
+                <div className='sprite'>
+                    <img className='image' src={`${data.sprites.front_default}`} alt="" />
+                </div>
+                <div>
+                <div className='info'>
+                    Name: {pokemon}
+                </div>
+                <div className='info'>
+                    Type: {data.types.length === 1 ? data.types[0].type.name : `${data.types[0].type.name}/${data.types[1].type.name}`}
+                </div>  
+                </div>
+
+            </div>
+        );}
+    else
+        return(
+            <></>
+        )
 }
